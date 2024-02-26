425eaa20db722777520b2cbdbc5e7ce7669ae3e8
James Newton <blackslate@lexogram.com>
2024-02-26 15:06:48 +0300

    Initial commit: a simple Express server with cors

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express Server for exploring CORS #
+
+A bare-bones demonstration of how to use CORS to limit access to resources on an Express server.
---
+++ app.js
@@ -0,0 +1,70 @@
+const PORT = 3000
+
+// Initialize the Express app
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log("origin:", origin);
+    callback(null, true)
+  }
+}
+
+app.use(cors(corsOptions))
+
+
+app.get('/', (req, res) => {
+  // Respond to a request with a timed message giving
+  // the URL to where the server received the request
+  const { protocol, hostname } = req
+  const message = `<pre>
+Connected to ${protocol}://${hostname}:${PORT}
+${Date()}
+</pre>`
+
+  res.send(message)
+})
+
+
+app.get('/headers', (req, res) => {
+  const message = `<pre>
+${JSON.stringify(req.headers, null, "  ")}
+</pre>`
+
+  res.send(message)
+})
+
+
+app.listen(PORT, optionalCallbackForListen) 
+
+
+//Print out some useful information in the Terminal
+function optionalCallbackForListen() {
+  // Check what IP addresses are used by your 
+  // development computer.
+  const nets = require("os").networkInterfaces()
+  const ips = Object.values(nets)
+  .flat()
+  .filter(({ family }) => (
+    family === "IPv4")
+  )
+  .map(({ address }) => address)
+
+  // ips will include `127.0.0.1` which is the
+  // "loopback" address for your computer. This
+  // address is not accessible from other
+  // computers on your network. The host name
+  // "localhost" can be used as an alias for
+  // `127.0.0.1`, so you can add that, too.
+  ips.unshift("localhost")
+
+  // Show in the Terminal the URL(s) where you
+  // can connect to your server  
+  const hosts = ips.map( ip => (
+    `http://${ip}:${PORT}`)
+  ).join("\n  ")
+  console.log(`Express server listening at:
+  ${hosts}`);
+}
